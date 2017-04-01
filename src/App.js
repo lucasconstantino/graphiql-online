b7708bd44b600d5259649b9463a0e1ea502640d0
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2017-04-01 01:46:06 -0300

    Added custom defaultQuery.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,51 @@ import { isUri } from 'valid-url'
 const options = { method: 'post', headers: { 'Content-Type': 'application/json' } }
 const endpoint = 'https://api.graph.cool/simple/v1/ciyz901en4j590185wkmexyex' // Initial
 
+const defaultQuery = `
+# Welcome to GraphiQL
+#
+# GraphiQL is an in-browser tool for writing, validating, and
+# testing GraphQL queries.
+#
+# Type queries into this side of the screen, and you will see intelligent
+# typeaheads aware of the current GraphQL type schema and live syntax and
+# validation errors highlighted within the text.
+#
+# GraphQL queries typically start with a "{" character. Lines that starts
+# with a # are ignored.
+#
+# An example GraphQL query might look like:
+#
+#     {
+#       field(arg: "value") {
+#         subField
+#       }
+#     }
+#
+# Keyboard shortcuts:
+#
+#       Run Query:  Ctrl-Enter (or press the play button above)
+#
+#   Auto Complete:  Ctrl-Space (or just start typing)
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Default endpoint is an instance of https://www.graph.cool/
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+query {
+  allUsers {
+    id
+    name
+  }
+}
+`
+
 export default class App extends React.Component {
   constructor (props) {
     super(props)
 
     this.state = {
+      defaultQuery,
       endpoint,
       fetcher: this.createFetcher(endpoint)
     }
@@ -49,7 +89,11 @@ export default class App extends React.Component {
   render () {
     return (
       <div id='container'>
-        <GraphiQL fetcher={ this.state.fetcher } ref={ this.setRef }>
+        <GraphiQL
+          ref={ this.setRef }
+          fetcher={ this.state.fetcher }
+          defaultQuery={ this.state.defaultQuery }
+        >
           <GraphiQL.Toolbar>
             <GraphiQL.Button
               label='Change endpoint'
